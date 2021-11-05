fc4331290c9461216ec77ef025b4842b1e3d8600
yustisiarni <56014900+yustisiarni@users.noreply.github.com>
2021-11-05 16:47:28 +0700

    Perbaikan coin

--- world2.gd
+++ world2.gd
@@ -6,11 +6,30 @@ extends Node
 # var b = "text"
 
 
+# Called when the node enters the scene tree for the first time
+
+var point = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Coinjml.UpdatePoint($"/root/VarPoint".point)
-	pass # Replace with function body.
+	point = $"/root/VarPoint".point
+#	$coin.connect("brain_keambil", self,"_on_TambahPoint")
+ # Replace with function body.
+	var kump_coins
+	kump_coins =$coins2.get_children()
+	$Coinjml.UpdatePoint(point)
+#	for i in range (0, kump_coins.size()):
+#		print(kump_coins[i].name)
 
+	for coin in kump_coins :
+		coin.connect("brain_keambil",self,"_on_TambahPoint")
+
+
+
+func _on_TambahPoint():
+	point += 10
+	$"/root/VarPoint".point = point
+	$Coinjml.UpdatePoint(point)
+	print(point)
 #func _on_TambahPoint():
 #	point += 10
 #	$"/root/VarPoint".point = point
