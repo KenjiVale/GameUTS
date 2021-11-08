79f1b90b8b1af6a6f7c80765f06f195816c8d48d
DESKTOP-2C9OG9G\Novia <1972049@Maranatha.ac.id>
2021-11-08 13:42:46 +0700

    ganti warna hover button

--- WIN.gd
+++ WIN.gd
@@ -6,9 +6,8 @@ extends Control
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$startButton.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
