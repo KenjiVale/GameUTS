6191f1c08dc65877d77259f245561128f0200637
yustisiarni <56014900+yustisiarni@users.noreply.github.com>
2021-11-08 18:35:21 +0700

    Update ParallaxBackground.gd

--- ParallaxBackground.gd
+++ ParallaxBackground.gd
@@ -11,10 +11,11 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	$ParallaxLayer.motion_offset.x -= 5
-	$ParallaxLayer2.motion_offset.x -= 5
-	$ParallaxLayer3.motion_offset.x -= 5
-	$ParallaxLayer4.motion_offset.x -= 5
+	var speed = 3
+	$ParallaxLayer.motion_offset.x -= speed
+	$ParallaxLayer2.motion_offset.x -= speed
+	$ParallaxLayer3.motion_offset.x -= speed
+	$ParallaxLayer4.motion_offset.x -= speed
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
